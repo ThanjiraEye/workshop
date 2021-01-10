fb522631e973155b0c7952a311f6a9e8169cfa0b
ThanjiraEye <thanjira.j@ku.th>
2021-01-10 11:10:40 +0700

    add new name.py file

---
+++ variables/name.py
@@ -0,0 +1,12 @@
+#สามารถประกาศได้
+myvar = "Eye"
+my_var = "Eye"
+_my_var = "Eye"
+myVar = "Eye"
+MYVAR = "Eye"
+myvar2 = "Eye"
+
+#ไม่สามารถประกาศได้
+2myvar = "Eye"
+my-var = "Eye"
+my var = "Eye"
