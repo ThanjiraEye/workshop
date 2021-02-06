0912f06d1425f4230029308716df327d77b826cb
ThanjiraEye <thanjira.j@ku.th>
2021-02-06 12:00:27 +0700

    add new 5 foder(tuple,dictionaries,operators,if_else,while_loop,for_loop,rang) and add new tuple 3 file , dictionaries 7 file , operators 11 file , if_else 8 file , while_loop 4 file , for_loop 6 file and rang 3 file

---
+++ operators/membership_op2.py
@@ -0,0 +1,7 @@
+dicObject = {"name": "Eye & Ice", "age": "21"}
+
+print("name" in dicObject)
+# output: True
+
+print("Eye & Ice" in dicObject)
+# output: False
---
+++ operators/membership_op4.py
@@ -0,0 +1,7 @@
+name_list = ["Thanjira", "Eye"]
+
+print("Eye" in name_list)
+# output: True
+
+print("Lucas" in name_list)
+# output: False
